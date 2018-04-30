accf8a52b697264bdceade283c97f0dd2f510234
gary wang <gary@equity.com>
2018-04-29 23:38:43 -0500

    fix format in scatter_plot

--- plotting/line_chart.py
+++ plotting/line_chart.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from sklearn import datasets
-from sklearn import model_selection
 import itertools as it
 import numpy as np
+
 
 # import matplotlib.patches as mpatches
 # import itertools as it
@@ -28,15 +28,15 @@ def plot_profile(X, y, names):
     >>> plot_profile(X, y, names)
     """
     # spilt the data into clusters by class label
-    clusters = {i:[] for i in set(y)}
+    clusters = {i: [] for i in set(y)}
     for i in range(len(y)):
         clusters[y[i]].append(i)
-    fig = plt.figure(figsize=(15, 10), dpi= 100, facecolor='w', edgecolor='k')
+    plt.figure(figsize=(15, 10), dpi=100, facecolor='w', edgecolor='k')
     # choose subplot
     ax = plt.subplot(1, 1, 1)
     # custom x ticks
     x = [i for i in range(len(names))]
-    plt.xticks(x, names, fontsize = 12)
+    plt.xticks(x, names, fontsize=12)
     # rotate xticks
     for label in ax.get_xmajorticklabels():
         label.set_rotation(30)
@@ -45,15 +45,16 @@ def plot_profile(X, y, names):
         # ax.plot(x,centers.tolist()[j],color='red')
         # plot data points in a cluster
         patches = []
-        colors = it.cycle(plt.cm.rainbow(np.linspace(0,1,len(clusters))))
+        colors = it.cycle(plt.cm.rainbow(np.linspace(0, 1, len(clusters))))
         for cls, clr in zip(clusters, colors):
             patches.append(mpatches.Patch(color=clr, label=cls))
             for i in clusters[cls]:
-                ax.plot(x, X[i], color=clr ,alpha=0.02, linewidth=2.0)
+                ax.plot(x, X[i], color=clr, alpha=0.02, linewidth=2.0)
         # set ylabel and title
         plt.title('profile for each individual')
     plt.legend(handles=patches)
     plt.show()
+
 
 def test_plot_profile():
     iris = datasets.load_iris()
--- plotting/scatter_plot.py
+++ plotting/scatter_plot.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from collections import Counter
-from sklearn import preprocessing
-from sklearn import datasets
-from sklearn import model_selection
+from sklearn.preprocessing import MinMaxScaler
+
 
 # import matplotlib.pyplot as plt
 def plot_2d(X1, y1, X2, y2, title1='dataset1', title2='dataset2'):
     """
-    This function is used to compare two datasets by make 2d-scatter plot for each of them.
+    This function is used to compare two datasets by
+    make 2d-scatter plot for each of them.
 
     Parameters
     ----------
@@ -25,6 +25,8 @@ def plot_2d(X1, y1, X2, y2, title1='dataset1', title2='dataset2'):
         title for plot1
     title2: str
         title for plot2
+
+    >>> plot_2d(X1, y1, X2, y2)
     """
     # Two subplots, unpack the axes array immediately
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
@@ -35,7 +37,12 @@ def plot_2d(X1, y1, X2, y2, title1='dataset1', title2='dataset2'):
     for ci in C:
         label = "Class " + str(ci)
         # plot dataset1
-        col = ax1.scatter(X1[y1 == ci, 0], X1[y1 == ci, 1], label=label, alpha=0.5)
+        col = ax1.scatter(
+            X1[y1 == ci, 0],
+            X1[y1 == ci, 1],
+            label=label,
+            alpha=0.5
+        )
         # plot dataset2
         ax2.scatter(X2[y2 == ci, 0], X2[y2 == ci, 1], label=label, alpha=0.5)
         colors.append(col)
@@ -51,16 +58,10 @@ def plot_2d(X1, y1, X2, y2, title1='dataset1', title2='dataset2'):
         ax.get_yaxis().tick_left()
         ax.spines['left'].set_position(('outward', 10))
         ax.spines['bottom'].set_position(('outward', 10))
-    fig.legend(colors, labels, loc='lower center', ncol=len(C), labelspacing=0.)
+    fig.legend(colors, labels, loc='lower center', ncol=len(C), labelspacing=0)
     plt.tight_layout(pad=3)
     plt.show()
 
-def test_plot_2d():
-    iris = datasets.load_iris()
-    X = iris.data[:, :2]  # we only take the first two features.
-    y = iris.target
-    X1, X2, y1, y2 = model_selection.train_test_split(X, y, test_size=0.5, random_state=42)
-    plot_2d(X1, y1, X2, y2)
 
 def plot_coincidence(y1, y2):
     """
@@ -95,14 +96,10 @@ def plot_coincidence(y1, y2):
 
     # use the scatter function
     fig, ax = plt.subplots(1, 1, figsize=(15, 10))
-    ax.scatter(x, y, s=z*2000, c=z, cmap="RdYlGn", alpha=0.4, edgecolors="black", linewidth=2)
+    ax.scatter(
+        x, y, s=z*2000, c=z, cmap="RdYlGn",
+        alpha=0.4, edgecolors="black", linewidth=2
+    )
     plt.xticks(range(min(x), math.ceil(max(x))+1))
     plt.yticks(range(min(y), math.ceil(max(y))+1))
     plt.show()
-
-def test_plot_coincidence():
-    y1 = [1, 2, 1, 2, 1, 3, 4, 5, 4, 4]
-    y2 = [3, 1, 3, 2, 2, 2, 1, 3, 1, 1]
-    plot_coincidence(y1, y2)
-    for i, label_y1 in enumerate(y1):
-        label_y2 = y2[i]
